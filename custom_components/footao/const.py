e8c26f0219cb9faed2b1426b0d3e26ac69cdf956
DomoBox <eedomusbox@gmail.com>
2026-08-28 14:00:38 +0200

    Update const.py

--- custom_components/footao/const.py
+++ custom_components/footao/const.py
@@ -55,21 +55,4 @@ MANIFEST_PATH = Path(__file__).parent / "manifest.json"
 with open(MANIFEST_PATH, encoding="utf-8") as f:
     INTEGRATION_VERSION: Final[str] = json.load(f).get("version", "0.0.0")
 
-DOMAIN: Final[str] = "Footao TV"
-
-# URL de base pour les ressources frontend
-URL_BASE: Final[str] = "/Footao TV"
-
-# Liste des modules JavaScript à enregistrer
-JSMODULES: Final[list[dict[str, str]]] = [
-    {
-        "name": "Footao Tv Game Card",
-        "filename": "footao-game-card.j",
-        "version": INTEGRATION_VERSION,
-    },
-    # Ajouter l'éditeur si nécessaire
-    {
-        "name": "Footao Tv Game Card",
-        "filename": "footao-game-card.j",
-        "version": INTEGRATION_VERSION,
    
